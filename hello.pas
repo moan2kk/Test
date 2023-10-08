1943037840a75c3d1d3f4051537b18e7c11cc261
Николай Стрельников <898894951342013@mail.ru>
2023-10-08 14:59:11 +0300

    Добавленно приветствие на 3 языках

--- hello.pas
+++ hello.pas
@@ -1,3 +1,10 @@
 ﻿begin
-  Print(Привет);
+  var n := ReadInteger('На каком языке ты говоришь?(1-Рус.яз,2-Inglish,3-Deutsch');
+  Assert(n in 1..3);
+  if n = 1 then
+    Print('Привет');
+  if n = 2 then
+    Print('Hello');
+  if n = 3 then
+    Print('Hallo');
 end.
